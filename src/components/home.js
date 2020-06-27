f9d7f54423eef80579425447fefdc487131cf912
lokesh900 <nanibook1@gmail.com>
2020-06-27 17:35:52 +0530

    first c

---
+++ src/components/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <hr />
+        <h4 style={{ textAlign: "center", color: "blue" }}>
+          Welcome To Tarvelo
+        </h4>
+        <hr />
+      </div>
+    );
+  }
+}
+
+export default Home;
